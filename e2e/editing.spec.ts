cf64d687c8fe4e4a1ef9fd8a1db766b72c4f7397
Jamie Kerr <jamie.kerr@nfocus.co.uk>
2023-04-18 13:58:22 +0100

    Refactor editing - saving changes tests

--- e2e/editing.spec.ts
+++ e2e/editing.spec.ts
@@ -5,28 +5,38 @@ test.beforeEach(async ({ page }) => {
     await page.goto('https://todomvc.com/examples/typescript-angular/#/')
 });
 
-test('Edit todo and save using blur', async ({ page }) => {
+test("Editing - saving changes by pressing enter key", async ({ page }) => {
     const angularHomepage = new AngularHomepage(page);
-    let oldText: string = 'Lorem';
-    let newText: string = 'Ipsum'
 
-    await angularHomepage.AddNewTodo(oldText);
-    // Exit the input box by blurring it.
-    await angularHomepage.EditTodo(oldText, newText, 'blur');
-
-    expect(await angularHomepage.checkTodoPresentByText(newText)).toBe(true);
-})
-
-test('Edit todo and save using enter', async ({ page }) => {
-    const angularHomepage = new AngularHomepage(page);
     let oldText: string = "Lorem";
-    let newText: string = 'Ipsum'
+    let newText: string = "Ipsum";
 
+    // Create new todo and edit it, saving by pressing enter key.
     await angularHomepage.AddNewTodo(oldText);
-    // Exit the input box by pressing the enter key.
     await angularHomepage.EditTodo(oldText, newText, 'enter');
 
+    // Verify that the todo's text is what we expect.
     expect(await angularHomepage.checkTodoPresentByText(newText)).toBe(true);
+
+    // Verify that the editing class has been removed.
+    
+})
+
+test("Editing - saving changes by blurring input textbox", async ({ page }) => {
+    const angularHomepage = new AngularHomepage(page);
+
+    let oldText: string = "Lorem";
+    let newText: string = "Ipsum";
+
+    // Create new todo and edit it, saving by pressing enter key.
+    await angularHomepage.AddNewTodo(oldText);
+    await angularHomepage.EditTodo(oldText, newText, 'blur');
+
+    // Verify that the todo's text is what we expect.
+    expect(await angularHomepage.checkTodoPresentByText(newText)).toBe(true);
+
+    // Verify that the editing class has been removed.
+
 })
 
 test('Edit todo and discard changes', async ({ page }) => {
@@ -51,22 +61,22 @@ test("Edit todo, don't change anything and discard edit", async ({ page }) => {
     expect(await angularHomepage.checkTodoPresentByText(oldText)).toBe(true);
 })
 
-test("Destroy todo by removing text then saving by blurring the input textbox", async ({ page }) => {
+test("Destroy todo by removing text then saving by pressing the escape button", async ({ page }) => {
     const angularHomepage = new AngularHomepage(page);
     let oldText: string = "Lorem";
 
     await angularHomepage.AddNewTodo(oldText);
-    await angularHomepage.EditTodo(oldText, '', 'blur');
+    await angularHomepage.EditTodo(oldText, '', 'escape');
 
     expect(await angularHomepage.checkAnyTodosPresent()).toBe(false);
 })
 
-test("Destroy todo by removing all non-whitespace, then saving by blurring the input textbox", async ({ page }) => {
+test("Destroy todo by removing all non-whitespace, then saving by pressing the escape button", async ({ page }) => {
     const angularHomepage = new AngularHomepage(page);
     let oldText: string = "Lorem";
 
     await angularHomepage.AddNewTodo(oldText);
-    await angularHomepage.EditTodo(oldText, '  	    ', 'blur');
+    await angularHomepage.EditTodo(oldText, '  	    ', 'escape');
 
     expect(await angularHomepage.checkAnyTodosPresent()).toBe(false);
 })
