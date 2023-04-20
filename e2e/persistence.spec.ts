c8227261ab05908f011ec63bd258e92c0b5d32a3
Jamie Kerr <jamie.kerr@nfocus.co.uk>
2023-04-20 15:54:02 +0100

    Kick start finding how todos are stored

--- e2e/persistence.spec.ts
+++ e2e/persistence.spec.ts
@@ -7,8 +7,23 @@ test.beforeEach(async ({ page }) => {
 
 test('Check editing mode isn\'t persisted on reload', async ({ page }) => {
     const angularHomepage: AngularHomepage = new AngularHomepage(page);
-    await angularHomepage.addOneTodo("Example");
-    await angularHomepage.EnterEditMode("Example");
+    await angularHomepage.addNewTodo("Example");
+    await angularHomepage.enterEditMode("Example");
     page.reload();
     expect(await angularHomepage.checkTodoBeingEditedByText("Example")).toBe(false);
+})
+
+test('Check todos are stored with correct keys', async ({ page }) => {
+    const angularHomepage: AngularHomepage = new AngularHomepage(page);
+    // let storage = await page.context().storageState({path: "localStorage"});
+    await angularHomepage.addNewTodo("Lorem");
+    
+    // Get the overall storage state of the browser.
+    let storageState = await page.context().storageState();
+    // Get the local storage.
+    let localStorage = await storageState.origins[0].localStorage[0];
+    // Find the value which should correspond to the todo we just added.
+    let storedTodo: string = localStorage.value;
+    console.log(storedTodo);
+
 })
