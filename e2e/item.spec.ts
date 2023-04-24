908adb09657a1b6ea653c3b59b5a293f1e5b7344
Tomasz Tlalka <tomasz.tlalka@nfocus.co.uk>
2023-04-24 14:08:16 +0100

    Semicolons and newline

--- e2e/item.spec.ts
+++ e2e/item.spec.ts
@@ -6,8 +6,6 @@ let lorem: string = "Lorem";
 test.beforeEach(async ({ page }) => {
     await page.goto('https://todomvc.com/examples/typescript-angular/#/')
     const toDo = new AngularHomepage(page);
-      
-
     await toDo.addNewTodo(lorem);
 });
 
@@ -15,17 +13,17 @@ test('Mark todo as complete', async ({ page }) => {
     const toDo = new AngularHomepage(page);
     await toDo.markAsCompletedByText(lorem);
     expect(await toDo.checkTodosCompletedByText(lorem)).toBe(true);
-})
+});
 
 test('Enable editing mode', async ({ page }) => {
     const toDo = new AngularHomepage(page);
     await toDo.enterEditMode(lorem);
     await page.getByRole('listitem').getByRole('textbox').fill('test');
     expect(await toDo.checkTodoBeingEditedByText("test")).toBe(true);
-})
+});
 
 test('Remove button shows on hover', async ({ page }) => {
     const toDo = new AngularHomepage(page);
     await toDo.hoverOverTodoByText(lorem);
     await expect(page.getByRole('button', { name: '×' })).toBeVisible();    
-})
+});
