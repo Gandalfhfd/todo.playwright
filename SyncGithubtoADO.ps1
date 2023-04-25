e66d2815b4939fa61b42397f7ced5cfc4bea0f60
Jamie Kerr <jamie.kerr@nfocus.co.uk>
2023-04-25 14:40:51 +0100

    Fix typo in remote pruning

--- SyncGithubtoADO.ps1
+++ SyncGithubtoADO.ps1
@@ -60,7 +60,7 @@ Write-Output '*****Git push secondary****'
 #git remote set-url origin $destURLSetURL
 git push secondary --all -f
 Write-Output '*****Git remote prune secondary****'
-git prune remote secondary
+git remote prune secondary
 Write-Output '**Azure Devops repo synced with Github repo**'
 Set-Location $stageDir
 if((Test-Path -path $githubDir))
