ae629054dc7f3e49f844f7f36d3fda20c2b18b7f
Jamie Kerr <jamie.kerr@nfocus.co.uk>
2023-04-25 14:39:44 +0100

    Hopefully make ADO pruned

--- SyncGithubtoADO.ps1
+++ SyncGithubtoADO.ps1
@@ -55,10 +55,12 @@ git remote rm secondary
 Write-Output '*****Git remote add****'
 git remote add --mirror=fetch secondary $destURL
 Write-Output '*****Git fetch origin****'
-git fetch --prune $sourceURL
+git fetch $sourceURL
 Write-Output '*****Git push secondary****'
 #git remote set-url origin $destURLSetURL
-git push secondary  --all -f
+git push secondary --all -f
+Write-Output '*****Git remote prune secondary****'
+git prune remote secondary
 Write-Output '**Azure Devops repo synced with Github repo**'
 Set-Location $stageDir
 if((Test-Path -path $githubDir))
