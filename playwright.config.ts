b03caf058d9c7ca101aeb700eecd2a80acef57e6
Jamie Kerr <jamie.kerr@nfocus.co.uk>
2023-04-26 11:31:26 +0100

    Modify playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['json', {outputFile: 'results.json'}]],
+  reporter: [['json', {outputFile: './results.json'}]],
   // reporter: [
   //   ['list'],
   //   [
