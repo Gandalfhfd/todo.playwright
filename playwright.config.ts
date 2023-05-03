5d184a0bbe57dad9cd66ed0b75e920f9270fb6e8
Jamie Kerr <jamie.kerr@nfocus.co.uk>
2023-05-03 09:51:03 +0100

    Switch to headless in azure-pipeline branch

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   reporter: [['junit', {outputFile: 'test-results/e2e-junit-results.xml'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
